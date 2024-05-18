f29ed45ebf6fab9d2e457d14b5430c8eb6f01d02
Maycon Douglas <mdrs.maycon@gmail.com>
2024-05-18 15:04:25 -0300

    feat: services for application

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -1,9 +1,29 @@
 import axios from 'axios';
+import { deleteUserLocalStorage } from '@/context/AuthProvider/util';
+import { message } from 'antd';
 
 export const Api = axios.create({
-  baseURL: 'https://api.homologation.cliqdrive.com.br/auth/',
+    baseURL: import.meta.env.VITE_API_URL,
     headers: {
         'Accept': 'application/json;version=v1_web',
         'Content-Type': 'application/json',
     },
 });
+
+/**
+ * Initializes the Axios instance with a custom response interceptor.
+ * @param {Function} onUnauthorized - Callback function to be called when a 401 response is received.
+ */
+export const initializeAxios = (onUnauthorized: { (): void; (): void; }) => {
+    Api.interceptors.response.use(
+        response => response,
+        error => {
+            if (error.response && error.response.status === 401) {
+                onUnauthorized(); // Calls the callback function to redirect
+                deleteUserLocalStorage();
+                message.error("Session expired! Login again.");
+            }
+            console.log(error);
+        }
+    );
+};
--- src/services/LoginService.ts
+++ src/services/LoginService.ts
@@ -1,25 +1,49 @@
 import { Api } from '@/services/Api';
+import { ILogin } from '@/interfaces/IUser';
 
-export async function login(email: string, password: string): Promise<any> {
+/**
+ * Sends a login request to the server.
+ * @param email The user's email for login.
+ * @param password The user's password for login.
+ * @returns The user information if the login is successful, or null if an error occurs.
+ */
+export async function LoginRequest(login: ILogin) {
     try {
-        const response = await Api.post('/login/', {
-            email,
-            password,
-        });
-    
+        const response = await Api.post('/login/', login);
         return response.data;
     } catch (error) {
         console.log(error);
-        return null;
+        throw error;
     }
 }
 
+
+/**
+ * Retrieves the user profile from the server.
+ * @returns The user's profile information.
+ */
 export async function getProfile() {
+    const userDataString = localStorage.getItem('u');
+    console.log(userDataString)
+
+    if (!userDataString) {
+        throw new Error("User data not found in local storage");
+    }
+
+    const userData = JSON.parse(userDataString);
+    const token = userData.token;
+
+    if (!token) {
+        throw new Error("Token not found in user data");
+    }
+
     try {
-        const response = await Api.get('/profile/');
+        const response = await Api.get('/profile/', {
+            headers: { 'Authorization': `Bearer ${token}` }
+        });
         return response.data;
     } catch (error) {
-        console.log(error);
-        return null;
+        console.error(error);
+        throw error;
     }
 }
